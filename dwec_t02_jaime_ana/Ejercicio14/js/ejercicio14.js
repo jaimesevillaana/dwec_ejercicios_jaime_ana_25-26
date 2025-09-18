ac0aa867c0197e9dbf38a91384cf743cf365c58a
Ana Jaime <anickajamesseville@outlook.es>
2025-09-18 23:10:43 +0200

    Ejercicio 14

--- dwec_t02_jaime_ana/Ejercicio14/js/ejercicio14.js
+++ dwec_t02_jaime_ana/Ejercicio14/js/ejercicio14.js
@@ -1,2 +1,20 @@
 console.log("T02 - Ejercicio 14");
 
+let num = parseInt(prompt("Introduce un número entero positivo:"));
+if (isNaN(num) || num <= 0) {
+    alert("Por favor, introduce un número entero positivo válido.");
+} else {
+    let sumaDivisores = 0;
+
+    for (let i = 1; i <= num / 2; i++) {
+        if (num % i === 0) {
+            sumaDivisores += i;
+        }   
+    }
+
+    if (sumaDivisores > num) {
+        alert(`${num} es un numero abundante.`);
+    } else {
+        alert(`${num} no es un numero abundante.`);
+    }   
+}
