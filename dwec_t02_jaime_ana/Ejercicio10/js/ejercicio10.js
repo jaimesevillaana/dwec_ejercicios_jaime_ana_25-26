0c6e36e79b4e80e92a111634d90cac89a72951bd
Ana Jaime <anickajamesseville@outlook.es>
2025-09-18 10:29:03 +0200

    Ejercicios 10 y 11

--- dwec_t02_jaime_ana/Ejercicio10/js/ejercicio10.js
+++ dwec_t02_jaime_ana/Ejercicio10/js/ejercicio10.js
@@ -1,21 +1,17 @@
 console.log("T02 - Ejercicio 10");
 
+let num = parseInt(prompt("Introduce un número:"));
+
 function factorial(n){
     if(n < 0){
         return "El factorial no está definido para números negativos";
-    }
-    if(n === 0 || n === 1){
+    } else if(n === 0 ||n === 1){
         return 1;
-    }
+    } else {
     let resultado = 1;
     for(let i = 2; i <= n; i++){
         resultado *= i;
-    }  
+    }   
     return resultado;
+    }
 }
-
-let numero = parseInt(prompt("Introduce un número:"));
-
-let resultado = factorial(numero);
-
-alert(`El factoial de ${numero} es: ${resultado}`);
