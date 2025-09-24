e615d4ebd866e11827977c1e01c8f86197fe495e
Ana Jaime <anickajamesseville@outlook.es>
2025-09-24 22:42:04 +0200

    Ejercicio 10

--- dwec_t03p01_jaime_ana/Ejercicio10/js/ejercicio10.js
+++ dwec_t03p01_jaime_ana/Ejercicio10/js/ejercicio10.js
@@ -1,3 +1,29 @@
-
+/*
+* Investiga sobre los métodos para redondear del objeto Math: 
+* "ceil()", "floor()" y "round()". 
+* ceil() -> redondea hacia arriba
+* floor() -> redondea a la baja
+* round() -> redondea al entero mas cercano
+* Haz un ejemplo donde los uses 
+* y se observe la diferencia entre cada uno de ellos.
+*/
 console.log("T03 - Ejercicio 10");
 
+let num1 = 4.1;
+let num2 = 4.5;
+let num3 = 4.9;
+
+console.log("Número:", num1);
+console.log("ceil:", Math.ceil(num1)); // 5
+console.log("floor:", Math.floor(num1)); // 4
+console.log("round:", Math.round(num1)); // 4
+
+console.log("Número:", num2);
+console.log("ceil:", Math.ceil(num2)); // 5
+console.log("floor:", Math.floor(num2)); // 4
+console.log("round:", Math.round(num2)); // 5
+
+console.log("Número:", num3);
+console.log("ceil:", Math.ceil(num3)); // 5
+console.log("floor:", Math.floor(num3)); // 4
+console.log("round:", Math.round(num3)); // 5
