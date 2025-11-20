d85f5b6f400277917b6ba3aa53d4506753d51861
Ana Jaime <anickajamesseville@gmail.com>
2025-11-20 09:07:23 +0100

    autores primeros pasos

--- dwec_t04p02_jaime_ana/Ejercicio1/js/ejercicio01_autores.js
+++ dwec_t04p02_jaime_ana/Ejercicio1/js/ejercicio01_autores.js
@@ -1,1 +1,31 @@
 console.log("T02 - Ejercicio 01");
+
+class Autores {
+    #listaAutores;
+
+    constructor() {
+        this.#listaAutores = [];
+    }
+
+    insertar(autor) {
+        if (!(autor instanceof Autor)) {
+            throw new Error("El parámetro no es una instancia de Autor");
+        }
+        this.#listaAutores.push(autor);
+    }
+
+    existeAutor(nombre) {
+        return this.#listaAutores.some(a => a.nombre === nombre);
+    }
+
+    buscar(nombre) {
+        return this.#listaAutores.find(a => a.nombre === nombre) || null;
+    }
+
+    listar() {
+        return this.#listaAutores.map(a => a.mostrarAutor()).join("\n");
+    }
+    get lista() {
+        return [...this.#listaAutores];
+    }
+}
