081aa755f3dd2e55060dead4aeedb661b5acbd86
Ana Jaime <161146852+jaimesevillaana@users.noreply.github.com>
2025-11-24 18:34:34 +0100

    construyendo la clase libro

--- dwec_t04p02_jaime_ana/Ejercicio1/js/ejercicio01_libros.js
+++ dwec_t04p02_jaime_ana/Ejercicio1/js/ejercicio01_libros.js
@@ -1,2 +1,34 @@
 console.log("T02 - Ejercicio 01");
 
+
+class Libros {
+
+    constructor() {
+        this.libros = [];
+    }
+
+
+    //getters
+
+    get libros() {
+        return [...this.libros];
+    }
+
+    addLibro(libro) {
+        if (!(libro instanceof Libro)) {
+            throw new Error("No es una instancia de Libro");
+        }
+        if (this.libros.findLibro(libro.isbn)) {
+            throw new Error("Ya existe un libro con ese ISBN");
+        }
+        this.#libros.push(libro);
+        console.log(`Libro con ISBN ${libro.isbn} añadido.`);
+    }
+
+    findLibro(isbn) {
+        if (!Util.validarEntero(isbn) || isbn <= 0) {
+            return this.#libros.find(libro => libro.isbn === isbn);
+        }
+    }  
+
+}
